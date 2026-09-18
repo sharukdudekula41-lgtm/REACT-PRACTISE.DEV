b47f5caa66c644948f161075e53b414cf81fef82
Dudekula Sharukh <sharukdudekula41@gmail.com>
2026-09-19 00:07:10 +0530

    Add files via upload

---
+++ Reusing the same component.jsx
@@ -0,0 +1,12 @@
+import student from"./Student.jsx";
+
+function APP() {
+    return (
+        <>
+        <Student name="Rahul" age={21} course="Python"/>
+        <Student name="Priya" age={20} course="Java"/>
+        <Student name="Sharukh" age={23} course="React"/>
+        </>
+    );
+}
+export default App;
