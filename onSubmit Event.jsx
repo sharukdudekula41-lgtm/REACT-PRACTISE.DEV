20f1850b74b0a7687ca8ac5732cafbc9ba0127b2
Dudekula Sharukh <sharukdudekula41@gmail.com>
2026-09-19 00:04:25 +0530

    Add files via upload

---
+++ onSubmit Event.jsx
@@ -0,0 +1,21 @@
+
+function App() {
+
+    function handleSubmit(event) {
+
+        event.preventDefault();
+
+        alert("Form Submitted");
+    }
+
+    return (
+
+        <form onSubmit={handleSubmit}>
+        <button type="submit">
+            Submit
+            </button>
+       </form>
+    );
+}
+
+export default App;
