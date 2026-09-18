b340f6eb47a22ae2717ccd618c326110504a2f74
Dudekula Sharukh <sharukdudekula41@gmail.com>
2026-09-18 22:48:39 +0530

    Add files via upload

---
+++ Destruring Props.jsx
@@ -0,0 +1,11 @@
+function student({ name, age, course}) {
+    return (
+        <>
+        <h2>{name}</h2>
+        <p>{age}</p>
+        <p>{Course}</p>
+        </>
+    );
+}
+
+export default student;
