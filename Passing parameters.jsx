0260b63f08ed873dbbf501988d474e5744c3dcd4
Dudekula Sharukh <sharukdudekula41@gmail.com>
2026-09-19 00:05:04 +0530

    Add files via upload

---
+++ Passing parameters.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+    return (
+        <button
+          onClick={() => alert("Hello React")}
+          >
+            Click
+            </button>
+    );
+}
+
+export default App;
