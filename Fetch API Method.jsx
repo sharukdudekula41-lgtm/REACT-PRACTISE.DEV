f5cfbf002ec261c9d10a66318a2cbc65812ff0e6
Dudekula Sharukh <sharukdudekula41@gmail.com>
2026-09-19 00:01:16 +0530

    Add files via upload

---
+++ Fetch API Method.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    }, []);
+
+    return (
+        <div>
+            <h2>Users</h2>
+
+            {users.map((user) => (
+                <p key={user.id}>
+                    {user.name}
+                </p>
+            ))}
+        </div>
+    );
+}
+
+export default Users;
