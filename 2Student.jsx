c65b10d3d33f3a9a7f01df7cec303f40f8003026
Dudekula Sharukh <sharukdudekula41@gmail.com>
2026-09-18 22:40:58 +0530

    Add files via upload

---
+++ 2Student.jsx
@@ -0,0 +1,11 @@
+function Student(props) {
+    return (
+       <>
+       <h2>Name: {props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Course: {props.course}</p>
+       </>
+    );
+
+}
+ export default Student;
